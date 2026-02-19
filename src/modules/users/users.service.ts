df65e7fe93f03848feff9a5870feb82ff7e37389
Arbaaz-Chouha <chouhanarbaaz4@gmail.com>
2026-02-19 10:50:05 +0530

    CI CD PRACTICE

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -99,4 +99,24 @@ export class UsersService {
       .status(200)
       .json({ message: 'User logged out successfully', success: true });
   }
+
+  //   @Get()
+  // findAll() {
+  //   return this.usersService.findAll();
+  // }
+
+  // @Get(':id')
+  // findOne(@Param('id') id: string) {
+  //   return this.usersService.findOne(+id);
+  // }
+
+  // @Patch(':id')
+  // update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+  //   return this.usersService.update(+id, updateUserDto);
+  // }
+
+  // @Delete(':id')
+  // remove(@Param('id') id: string) {
+  //   return this.usersService.remove(+id);
+  // }
 }
